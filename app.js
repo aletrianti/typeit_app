8e53603b119698a88ad66d2def36fbbf4b910e47
Alessia Trianti <alestrianti@gmail.com>
2019-09-19 17:49:33 +0200

    Now successfully connects to db

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const passportLocal = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
+const config = require('config');
 
 // Require secret keys
-// const db = require('./config/dbKeys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to DB
-// mongoose
-//     .connect(db, { useNewUrlParser: true })         // Connects to the database with the key specified in config/keys.js
-//     .then(() => console.log('Connected to db'))     // Connects and shows "Connected to db" in the console
-//     .catch(err => console.log(err));                // If there is an error, it gets displayed in the console
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })       // Connects to the database with the key specified in config/keys.js
+    .then(() => console.log('Connected to db'))                             // Connects and shows "Connected to db" in the console
+    .catch(err => console.log(err));                                        // If there is an error, it gets displayed in the console
 
 // Start the server on a specific port OR on port 3000 and show a message in the console
 const port = process.env.PORT || 3000;
