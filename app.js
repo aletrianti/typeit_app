a66b8dc99a1f60fb3a85e3b89a3efc3fef874859
Alessia Trianti <alestrianti@gmail.com>
2019-09-19 18:09:56 +0200

    Set up statement for serving static assets in production

--- app.js
+++ app.js
@@ -5,15 +5,27 @@ const passport = require('passport');
 const passportLocal = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 const config = require('config');
+const path = require('path');
 
-// Require secret keys
+// Require mongoURI key
 const db = config.get('mongoURI');
 
 // Connect to DB
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })       // Connects to the database with the key specified in config/keys.js
-    .then(() => console.log('Connected to db'))                             // Connects and shows "Connected to db" in the console
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })       // Connect to the database with the "mongoURI" key specified in config/default.json
+    .then(() => console.log('Connected to db'))                             // Connect and show "Connected to db" in the console
     .catch(err => console.log(err));                                        // If there is an error, it gets displayed in the console
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set the "build" folder as the static folder to use in production
+    app.use(express.static('client/build'));
+
+    // Set "index.html" as the index page in production
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // Start the server on a specific port OR on port 3000 and show a message in the console
 const port = process.env.PORT || 3000;
