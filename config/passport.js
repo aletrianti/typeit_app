6a5fa9991ccaa8b94f76a8b4e87f7af8f9822db1
Alessia Trianti <alestrianti@gmail.com>
2019-10-13 23:51:13 +0200

    Fixed authentication

--- config/passport.js
+++ config/passport.js
@@ -3,31 +3,28 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports = function(passport) {
-    passport.use(new LocalStrategy({ 
-        usernameField: 'email',
-        passReqToCallback : true 
-    }, (email, password, done) => {
-        // Check if the user does not exist
-        User.findOne({ email })
-            .then(user => {
-                if (!user) {
-                    return done(null, false, { msg: 'Invalid credentials' });
-                }
-        
-                // Compare the user's encrypted password with the one inserted in the login form
-                const passwordsMatch = bcrypt.compare(password, user.password);
-        
-                if (!passwordsMatch) {
-                    return done(null, false, { msg: 'Invalid credentials' });
-                }
-                
-                return done(null, user);
-            })
-            .catch(err => {
-                return done(null, false, { msg: err });
-            });
-    }));
+    passport.use(
+        new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+            // Check if the user exists
+            User.findOne({ email: email })
+                .then(user => {
+                    if (!user) {
+                        return done(null, false, { message: 'Invalid credentials' });
+                    }
 
+                    // Check if the passwords match
+                    bcrypt.compare(password, user.password, (err, passwordsMatch) => {
+                        if (err) throw err;
+                        if (passwordsMatch) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false, { message: 'Invalid credentials' });
+                        }
+                    });
+                });
+        })
+    );
+    
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const { check, validationResult } = require("express-validator");
+// const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
@@ -19,82 +19,51 @@ router.get('/register', (req, res) => {
 
 // POST request 
 // Register a new user to the '/register' route
-// Use 'express-validator' for validation purposes (check each field)
-// If there is an error, send a 400 status and a message to the user
-// If there are no errors, check if the user already exists, save the values entered in the register form by the user, encrypt the password and return a token
-router.post('/register', [
-    // The firstName field must not be empty
-    check('firstName', 'First name is required.').not().isEmpty(),
+router.post('/register', (req, res) => {
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
+    let errors = [];
 
-    // The lastName field must not be empty
-    check('lastName', 'Last name is required.').not().isEmpty(),
+    if (!firstName || !lastName || !email || !password || !confirmPassword) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
 
-    // The email field must not be empty
-    check('email', 'Email is required.').not().isEmpty(),
+    if (password != confirmPassword) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
 
-    // The email must be valid
-    check('email', 'Please, enter a valid email.').isEmail(),
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be at least 6 characters' });
+    }
 
-    // The password field must not be empty
-    check('password', 'Password is required.').not().isEmpty(),
-
-    // The password must be at least 5 characters
-    check('password', 'Your password must be at least 5 characters.').isLength({ min: 5 }),
-
-    // The confirmPassword field must not be empty
-    check('confirmPassword', 'You must confirm your password.').not().isEmpty(),
-
-    // confirmPassword must be equal to password
-    check('confirmPassword', 'The passwords do not match.').custom((value, { req }) => value === req.body.password)
-], (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    } 
-
-    const {
-        firstName,
-        lastName,
-        email,
-        password
-    } = req.body;
-
-    // Check if the user already exists
-    User.findOne({ email: email })
-        .then(user => {
+    // If there are any errors, render the register page again with all the values
+    if (errors.length > 0) {
+        res.render('register', { errors, firstName, lastName, email, password, confirmPassword });
+    } else {
+        User.findOne({ email: email }).then(user => {
             if (user) {
-                // errors.array() returns an array of objects. The message can be extracted from "msg"
-                // In order to display all the errors in the same way in the UI, a message for this specific error is defined manually
-                return res.status(400).json({ errors: [ { msg: 'This email has already been used.' } ] });
-            }
-        
-            // Create a new user
-            const newUser = new User({
-                firstName,
-                lastName,
-                email,
-                password
-            });
-        
-            // Encrypt the password
-            // First create the salt, then the hash
-            // The hash will be saved as the user's password in the database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) {
-                        return res.status(400).json({ errors: [ { msg: err } ] });;
-                    }
-                    newUser.password = hash;
-                    newUser.save()
-                    .then(user => {
-                        req.flash('success', 'You are now registered!');
-                        res.redirect('/dashboard');
-                    })
-                    .catch(err => console.log(err));
+                // If the user already exists, render the register page again with all the values
+                errors.push({ msg: 'Email already exists' });
+                res.render('register', { errors, firstName, lastName, email, password, confirmPassword });
+            } else {
+                // Create a new user
+                const newUser = new User({ firstName, lastName, email, password });
+          
+                // Encrypt the password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+
+                        // If there are no errors, save the hashed password as the user's password
+                        newUser.password = hash;
+                        // Save and redirect to the login page if everything is okay
+                        newUser.save()
+                            .then(user => { res.redirect('/login'); })
+                            .catch(err => console.log(err));
+                    });
                 });
-            });
+            }
         });
+    }
 });
 
 // GET request 
@@ -108,8 +77,7 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
         successRedirect: '/dashboard',
-        failureRedirect: '/login',
-        failureFlash: true
+        failureRedirect: '/login'
     })(req, res, next);
 });
 
