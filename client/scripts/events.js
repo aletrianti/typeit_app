b451f40be223fe72da9907f7fd999154a587fc6c
Alessia Trianti <alestrianti@gmail.com>
2019-10-30 11:29:58 +0100

    Modified forms + added share note dialog and btn

--- client/scripts/events.js
+++ client/scripts/events.js
@@ -10,9 +10,9 @@ function addNewNote() {
 
 // Hide the "create note form" by clicking on the element with the "hidden-note-form" id
 function hideNoteForm() {
-    const newCategoryForm = document.getElementById("app-dashboard-container-create-note");
+    const newNoteForm = document.getElementById("app-dashboard-container-create-note");
 
-    newCategoryForm.classList.add("hidden-note-form");
+    newNoteForm.classList.add("hidden-note-form");
 }
 
 // If the span element with the "add-category" id is clicked, display a form in order to add a category
@@ -29,8 +29,22 @@ function hideCategoryForm() {
     newCategoryForm.classList.add("hidden-category-form");
 }
 
+// If the element with the "app-dashboard-container-edit-note" is clicked, remove the "hidden-note-form" from that element
 function openNote() {
     const editNoteForm = document.getElementById("app-dashboard-container-edit-note");
 
     editNoteForm.classList.remove("hidden-note-form");
+}
+
+// If the "share" button is clicked...
+function shareNote() {
+    const shareNoteDialog = document.getElementById("app-share-note-dialog-container");
+
+    shareNoteDialog.classList.remove("hidden-note-form");
+}
+
+function hideDialog() {
+    const shareNoteDialog = document.getElementById("app-share-note-dialog-container");
+
+    shareNoteDialog.classList.add("hidden-note-form");
 }
