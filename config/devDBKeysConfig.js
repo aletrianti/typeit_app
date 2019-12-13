c4b62bf28d6afad525fc73c2b4eb97ec966dc69d
Alessia Trianti <alestrianti@gmail.com>
2019-12-13 22:35:33 +0100

    Deleted more lines from .gitignore

---
+++ config/devDBKeysConfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // mongoURI: 'mongodb://localhost:27017/typeit_db'
+    mongoURI: 'mongodb+srv://admin:admin123@typeit-lnmle.mongodb.net/typeit_dev_db?retryWrites=true&w=majority'
+};
---
+++ config/prodDBKeysConfig.js
@@ -0,0 +1,4 @@
+// Export this object as a module
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:admin123@typeit-lnmle.mongodb.net/typeit_db?retryWrites=true&w=majority'
+};
